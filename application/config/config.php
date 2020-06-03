3cf374f28d8f7e61e9a628af66da02d2141e3d3b
kabhishek18 <kabhishek18@gmail.com>
2020-06-03 11:41:24 +0530

    Chnage seen to live

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://192.168.0.102/sites/vaidaan/';
+$config['base_url'] = 'http://localhost/sites/vaidaan/';
 
 /*
 |--------------------------------------------------------------------------
